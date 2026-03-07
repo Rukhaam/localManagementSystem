2b94a8247afb74fc067fb45bb313b54139ec18d0
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-03-08 00:09:19 +0530

    Intercept DNS lookup to force IPv4 and switch to port 587

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,23 +1,28 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const sendEmail = async (options) => {
-//   console.log("Using Email:", process.env.SMTP_MAIL);
-//     console.log("Password length:", process.env.SMTP_PASSWORD ? process.env.SMTP_PASSWORD.length : "UNDEFINED!");
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    service: process.env.SMTP_SERVICE,
+    host: "smtp.gmail.com",
+    port: 587,             // This will only work if 'service' is completely removed!
+    secure: false,         
+    requireTLS: true,      
     auth: {
       user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
+      pass: process.env.SMTP_PASSWORD, 
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
   const mailOptions = {
     from: process.env.SMTP_MAIL,
     to: options.email,
     subject: options.subject,
-    html: options.html,
+    html: options.message,
   };
 
   await transporter.sendMail(mailOptions);
