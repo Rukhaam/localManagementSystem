8602bde01b2448781437048b8a959dcad676cf44
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-03-08 00:26:56 +0530

    resend

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,30 +1,23 @@
-import nodemailer from "nodemailer";
+import { Resend } from 'resend';
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    family: 4, 
-    auth: {
-      user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>', 
+      to: [options.email],
+      subject: options.subject,
+      html: options.message,
+    });
 
-  const mailOptions = {
-    from: `"Summit Digital" <${process.env.SMTP_MAIL}>`,
-    to: options.email,
-    subject: options.subject,
-    html: options.message,
-  };
-
-  await transporter.sendMail(mailOptions);
+    console.log("✅ Email successfully sent via Resend API!", data);
+    return data;
+  } catch (error) {
+    console.error("🚨 RESEND API ERROR:", error);
+    throw error;
+  }
 };
