97aa8947cd8d0fdd9c870a8c126f214d41575917
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-03-07 20:35:27 +0530

    fix nth

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,26 +1,25 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import nodemailer from "nodemailer";
 
 export const sendEmail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        service: process.env.SMTP_SERVICE,
-        auth: {
-            user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
 
-    const mailOptions = {
-        from: `Local Services <${process.env.SMTP_MAIL}>`,
-        to: options.email,
-        subject: options.subject,
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: options.email,
+    subject: options.subject,
+    html: options.message, 
+  };
 
-        html: options.message, 
-    };
-
-    await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 };
