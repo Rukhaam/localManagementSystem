8678c879db64f53879dc2c7e2ef3e5623ef0a16c
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-03-08 00:17:50 +0530

    smtp issue

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -6,12 +6,13 @@ dotenv.config();
 export const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,             // This will only work if 'service' is completely removed!
-    secure: false,         
-    requireTLS: true,      
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    family: 4, 
     auth: {
       user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD, 
+      pass: process.env.SMTP_PASSWORD,
     },
     tls: {
       rejectUnauthorized: false
@@ -19,7 +20,7 @@ export const sendEmail = async (options) => {
   });
 
   const mailOptions = {
-    from: process.env.SMTP_MAIL,
+    from: `"Summit Digital" <${process.env.SMTP_MAIL}>`,
     to: options.email,
     subject: options.subject,
     html: options.message,
